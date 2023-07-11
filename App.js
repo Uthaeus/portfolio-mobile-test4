08ec50f18f990c9464b0163ad7d56e8f6c1e3145
Uthaeus <romanlavery@gmail.com>
2023-07-11 10:43:39 -0600

    installed drawer navigation, still breaking on reanimated

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 import ContactScreen from './screens/ContactScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function BlogStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={ HomeScreen } />
+            <Stack.Screen name="About" component={ AboutScreen } />
+            <Stack.Screen name="Contact" component={ ContactScreen } />
+        </Stack.Navigator>
+    );
+}
 
 export default function App() {
   return (
